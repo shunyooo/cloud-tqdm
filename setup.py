da1696ee7008f1a75cbefeb380439230eb4f5393
Syunyo Kawamoto <syunyo1028@yahoo.co.jp>
2020-07-09 14:46:23 +0900

    update version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-
 readme = ""
 with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
     name="cloud-tqdm",
-    version='0.0.1',
+    python_requires=">=3.6",
+    version="0.0.2",
     description="Visualize the progress of a Python script on the web.",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -15,11 +15,7 @@ setup(
     author="shunyooo",
     author_email="shunyo.kawamoto@gmail.com",
     license="Apache License 2.0",
-    packages=[
-        "cloud_tqdm",
-    ],
-    url='https://github.com/shunyooo/cloud-tqdm',
-    install_requires=[
-        "tqdm"
-    ],
+    packages=["cloud_tqdm",],
+    url="https://github.com/shunyooo/cloud-tqdm",
+    install_requires=["tqdm"],
 )
